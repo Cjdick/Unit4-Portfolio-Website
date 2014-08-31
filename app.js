1952405de39060a53c6dccfc55fc67c107339368
Cjdick <chrisdueck87@gmail.com>
2014-08-31 16:05:35 -0700

    Basic functional version to review with Jonathan
    Pending update to bxSlider images

--- app.js
+++ app.js
@@ -5,5 +5,14 @@ $(document).ready( function() {
 		auto: true,
   		autoControls: true
 	});
+	var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+	var width = (w - 1000)/2-8.5;
+	$('.side-spacer').css("width",width)
+
+	$(window).resize(function(){
+		w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+		width = (w - 1000)/2-8.5;
+		$('.side-spacer').css("width",width)
+	});
 });
 
